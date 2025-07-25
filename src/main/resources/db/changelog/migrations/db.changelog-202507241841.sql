6beeaad723238f48d0ee277b4f665a9bfde79546
Hudson Reis <hudsonpr09@gmail.com>
2025-07-25 13:10:18 -0300

    hotfix: corrigindo migrations

--- src/main/resources/db/changelog/migrations/db.changelog-202507241841.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202507241841.sql
@@ -6,7 +6,6 @@ CREATE TABLE cards(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    card_order INT NOT NULL,
     board_column_id BIGINT NOT NULL,
 
     CONSTRAINT fk_board_column_id__boards_columns FOREIGN KEY (board_column_id) REFERENCES boards_columns(id) ON DELETE CASCADE
--- src/main/resources/db/changelog/migrations/db.changelog-202507241845.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202507241845.sql
@@ -4,10 +4,10 @@
 
 CREATE TABLE cards_blocks(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    block_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    block_reason VARCHAR(255) NOT NULL,
-    unblock_at TIMESTAMP NULL,
-    unblock_reason VARCHAR(255) NULL,
+    blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    blocked_reason VARCHAR(255) NOT NULL,
+    unblocked_at TIMESTAMP NULL,
+    unblocked_reason VARCHAR(255) NULL,
     card_id BIGINT NOT NULL,
 
     CONSTRAINT fk_card_id__cards FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE
